15efc201809a165e4468c5a34bdc337a60594066
narek0495 <44174053+narek0495@users.noreply.github.com>
2018-11-03 01:19:40 +0400

    Meta changes

--- resources/views/hand/step1.blade.php
+++ resources/views/hand/step1.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.main')
 
 @section('slider')
-<div class="container">
+<div class="dis container">
     <div class="row">
-      <div class="col-xl-12 ">
+      <div class="more col-xl-12 ">
         
         <div class="col-md-12 roundeds">
           <img class="big-logo rounded mx-auto d-block center-block" src="{{asset('images/big.png')}}">
@@ -11,21 +11,21 @@
         </div>
 
 
-        <div class="col-md-12 roundeds">
+        <div class="col-md-12 col-xl-1 roundeds">
 
             <h3 class="slogan">
              <div class="pass"> ԱՎԵԼԱՑՐՈւ՛, <br>
                 ԿՅԱՆՔ<br>
         <div class="active">ԲՆՈւԹՅԱՆԸ</div><br> </h3>
-        <div class="col-md-10 quote">
-            «  Մարդիկ հավանաբար չգիտեն, որ 60 կիլոգրամ
+        <div class="col-md-10 col-xl-8 quote">
+          <pre> «  Մարդիկ հավանաբար չգիտեն, որ 60 կիլոգրամ
               թուղթը  արժե  մեկ  ծառ։  Թղթի  վերամշակման
               ուղղությամբ  այսօր քիչ մարդիկ են աշխատում։
               Հանձնելով՝  որպես   աղբ  դիտարկվող  թուղթը,
-              դուք   կկարողանաք  կյանք  տալ  բնությանը ։ »
+              դուք   կկարողանաք  կյանք  տալ  բնությանը ։ »</pre>
             </div>
 
-        <img class="fly" src="{{asset('evertree/paperfly.png')}}"/>
+        <img class="col-xl-9 fly" src="{{asset('evertree/paperfly.png')}}"/>
         </div>
       </div>
     </div>
@@ -35,7 +35,7 @@
 
 
 @section('menu')
-<div  style="margin-top:6px;" class="col-sm-5 col-md-8 nav justify-content">
+<div  style="margin-top:6px;" class="ttt col-xl-12 col-sm-5 col-md-8 nav justify-content">
 
                                                               
 
@@ -43,7 +43,7 @@
             <h2 class="active text1">Թղթի հանձնում</h2>  
     </a>
 
-    <a style="text-decoration: none;" class="p-2 text-muted" href="/post">
+    <a style="text-decoration: none;" class="p-2 text-muted" href="/news">
       <h2 class="text1">Նորություններ</h2>  
 </a>
 
@@ -65,7 +65,7 @@
 
 @else
 <a style="text-decoration: none;" class="p-2 text-muted" href="/gardens">
-  <h2 class="text1">Այգիներ</h2>  
+  <h2 class="tt2 text1">Այգիներ</h2>  
 </a>
 
 @endif
@@ -80,7 +80,7 @@
 
 
     <div class="choose">
-       <div class="container col-md-9 col-xl-9 row-eq-height top">
+       <div class="box1 container col-md-9 col-xl-11 row-eq-height top">
 
       <div class="qur1 col-md-3 bg1 paper  activediv text-center">
           <img class="tp type" src="evertree/activetype.png"/>
@@ -143,7 +143,7 @@
                     @endif
                
             </div>
-          <button type="submit" class="batright" >ՀԱՋՈՐԴԸ ></button>
+          <button type="submit" class="bal but float-right btn btn-success" >ՀԱՋՈՐԴԸ ></button>
 
           </form>
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -86,7 +86,7 @@
                                         <div class="header-wrap">
                                       
                                       
-                                          <button class="window-toggle contact center-block" data-target="#message-window" style="
+                                          <button class="my window-toggle contact center-block" data-target="#message-window" style="
                                           position: absolute;
                                           left: -34px;
                                           right: auto;
@@ -153,7 +153,7 @@
                                                                    @else
 
 
-                                                                   <div class="col-sm-5 col-md-4 nav justify-content-end">
+                                                                   <div class="kom col-sm-5 col-md-4 nav justify-content-end">
                                                                         <a class="p-2 text-muted" href="{{ route('login') }}"><h2 class="text1">Մուտք</h2></a>
                                                                         <a class="p-2 text-muted" href="{{ route('register') }}"><h2 class="text1">Գրանցվել</h2></a>
                                                                         
@@ -197,7 +197,7 @@
   <div class="window-inner">
       <section class="section dark">
           <div class="section-content">
-              <div class="container">
+              <div class=" gscontainer">
                   <!-- Section Header -->
                   <div class="section-header">
                     <img src="http://127.0.0.1:8000/images/big.png" alt="" class="section-header-image" style="
@@ -226,17 +226,17 @@
           
 
 <!-- JS Plugins -->
-<script src="{{asset('assets/plugins/jquery/dist/jquery.min.js')}}"></script>
+<script src="assets/plugins/jquery/dist/jquery.min.js"></script>
 
-<script src="{{asset('assets/plugins/jquery.appear/jquery.appear.js')}}"></script>
-<script src="{{asset('assets/plugins/jquery.localscroll/jquery.localScroll.min.js')}}"></script>
-<script src="{{asset('assets/plugins/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
-<script src="{{asset('assets/plugins/slick-carousel/slick/slick.min.js')}}"></script>
-<script src="{{asset('assets/plugins/waypoints/lib/jquery.waypoints.min.js')}}"></script>
-<script src="{{asset('assets/plugins/jquery-validation/dist/jquery.validate.min.js')}}"></script>
-<script src="{{asset('assets/plugins/typed.js/dist/typed.min.js')}}"></script>
-<script src="{{asset('assets/plugins/snapsvg/dist/snap.svg-min.js')}}"></script>
-<script src="{{asset('assets/js/core.js')}}"></script>
+<script src="assets/plugins/jquery.appear/jquery.appear.js"></script>
+<script src="assets/plugins/jquery.localscroll/jquery.localScroll.min.js"></script>
+<script src="assets/plugins/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="assets/plugins/slick-carousel/slick/slick.min.js"></script>
+<script src="assets/plugins/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="assets/plugins/jquery-validation/dist/jquery.validate.min.js"></script>
+<script src="assets/plugins/typed.js/dist/typed.min.js"></script>
+<script src="assets/plugins/snapsvg/dist/snap.svg-min.js"></script>
+<script src="assets/js/core.js"></script>
 
         </body>
 </html>
